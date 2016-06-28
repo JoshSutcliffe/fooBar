93953f01077952add8e0f5e86723140424d07976
JoshSutcliffe <joshclement12@gmail.com>
2016-06-28 18:06:21 +0100

    finished

---
+++ README.md
@@ -0,0 +1,5 @@
+# Coding Challenge
+
+My attempt at taking on the coding challenge here https://projecteuler.net/problem=1
+
+Turns out it wasn't foobar
--- app.js
+++ app.js
@@ -1,1 +1,13 @@
-console.log("connected")
+var fooBar = function(num) {
+  var sum = 0;
+  for (var i = 0; i < num; i++) {
+    if (i % 3 === 0) {
+      sum+=i;
+    } else if (i % 5 === 0) {
+      sum+=i;
+    }
+  }
+  console.log(sum);
+}
+
+fooBar(1000);
